11b3a3dc95e919d490f6bff73800fbe0830d6a90
Sam Sterckval <sam@edgise.com>
2021-03-15 23:02:58 +0100

    added hello_world + test

--- edgine/edgine.py
+++ edgine/edgine.py
@@ -1,1 +1,9 @@
 """Main module."""
+
+
+def hello_world():
+    return("Get Edgy!")
+
+
+def say_hello():
+    print(hello_world())
--- tests/test_edgine.py
+++ tests/test_edgine.py
@@ -13,9 +13,16 @@ class TestEdgine(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.hello_message = "Get Edgy!"
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_000_something(self):
         """Test something."""
+
+    def test_001_hello_world(self):
+        output = edgine.hello_world()
+        print(output)
+        print(self.hello_message)
+        assert(output == self.hello_message)
